26475698e1f4e00d9a3610986347d1e74fb4875b
Morgan-Mischo <46653585+Morgan-Mischo@users.noreply.github.com>
2019-02-04 18:10:59 -0700

    Update AnagramCheckerTests.java

--- AnagramCheckerTests.java
+++ AnagramCheckerTests.java
@@ -7,8 +7,57 @@ import org.junit.jupiter.api.Test;
 class AnagramCheckerTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void sortString_AppleTest ()
+	{
+		String a = "apple"; 
+		String expected = "aelpp"; 
 
+		assertEquals(expected, AnagramChecker.sort(a)); 
+	}
+	
+	@Test
+	void sortString_UppercaseTest ()
+	{
+		String a = "AppLe"; 
+		String expected = "aelpp"; 
+		
+		assertEquals(expected, AnagramChecker.sort(a)); 
+	}
+	
+	@Test
+	void sortString_Numbers ()
+	{
+		String a = "98765"; 
+		String expected = "56789"; 
+		
+		assertEquals(expected, AnagramChecker.sort(a)); 
+	}
+	
+	
+	@Test
+	void sortString_Long ()
+	{
+		String a = "zzzzyyyyxxxxwwwwvvvvuuuuttttssssrrrrqqqqppppoooonnnnmmmmllllkkkkjjjjiiiihhhhggggffffeeeeddddccccbbbbaaaa"; 
+		String expected = "aaaabbbbccccddddeeeeffffgggghhhhiiiijjjjkkkkllllmmmmnnnnooooppppqqqqrrrrssssttttuuuuvvvvwwwwxxxxyyyyzzzz"; 
+		
+		assertEquals(expected, AnagramChecker.sort(a)); 
+	}
+	
+	@Test
+	void areAnagrams_True ()
+	{
+		String a = "fringe";
+		String b = "finger";
+		
+		assertTrue(AnagramChecker.areAnagrams(a, b));
+	}
+	
+	@Test
+	void areAnagrams_False ()
+	{
+		String a = "Human";
+		String b = "hummus";
+		
+		assertFalse(AnagramChecker.areAnagrams(a, b));
+	}
 }
